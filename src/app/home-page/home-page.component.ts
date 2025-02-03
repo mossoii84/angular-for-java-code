ffd68e846bf97997c8c7ff7d1fd5ce78237c787d
mossoii84 <mossomkh@gmail.com>
2025-02-03 21:01:58 +0100

    propre code

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -8,86 +8,83 @@ import { Store } from '@ngrx/store';
 import { TasksActions, TasksListActions } from '../state/tasks.actions';
 import { TasksListService } from '../services/tasks-list.service';
 import { Observable, map } from 'rxjs';
-import { selectTaskList} from '../state/tasks.selectors';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { selectTaskList } from '../state/tasks.selectors';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 import { DatePickerModule } from 'primeng/datepicker';
-import { MessageModule } from 'primeng/message';
-import { InputIconModule } from 'primeng/inputicon';
-import { IconFieldModule } from 'primeng/iconfield';
 import { TaskPriority } from '../enum/taskpriority.enum';
 import { SelectModule } from 'primeng/select';
 import { TaskStatus } from '../enum/taskstatus.enum';
 
-
-
 @Component({
   selector: 'app-home-page',
-  imports: [CommonModule, ButtonModule, TableModule, DialogModule, ReactiveFormsModule, FormsModule, DatePickerModule, MessageModule, 
-    IconFieldModule, InputIconModule, SelectModule],
+  imports: [
+    CommonModule,
+    ButtonModule,
+    TableModule,
+    DialogModule,
+    ReactiveFormsModule,
+    FormsModule,
+    DatePickerModule,
+    SelectModule,
+  ],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent implements OnInit {
-
-
-
-
-
   serviceTasksList = inject(TasksListService);
-  store= inject(Store);
+  store = inject(Store);
 
   tasks$: Observable<any> | undefined;
-  taskslist: Task[]=[];
+  taskslist: Task[] = [];
   visible: boolean = false;
   searchQuery: string = '';
   taskPriority = TaskPriority;
   taskStatus = TaskStatus;
 
-  //add new 
+  //form 
   profileForm = new FormGroup({
-    id: new FormControl('', [Validators.required, Validators.minLength(1), Validators.pattern(/^[0-9]+$/)]),
+    id: new FormControl('', [
+      Validators.required,
+      Validators.minLength(1),
+      Validators.pattern(/^[0-9]+$/),
+    ]),
     title: new FormControl('', [Validators.required, Validators.minLength(3)]),
     discription: new FormControl('', [Validators.minLength(5)]),
     datelimite: new FormControl<any>(''),
     priority: new FormControl('', [Validators.required]),
     status: new FormControl('', [Validators.required]),
-    user: new FormControl('', [Validators.required, Validators.minLength(3)])
+    user: new FormControl('', [Validators.required, Validators.minLength(3)]),
   });
-
-
-
-
 
   onAdd(form: FormGroup) {
     const task: Task = form.value;
     console.log('Task:', task);
-      this.store.dispatch(TasksActions.addTask({ task })); 
-      form.reset();
-      this.visible = false;
+    this.store.dispatch(TasksActions.addTask({ task }));
+    form.reset();
+    this.visible = false;
   }
-  
+
   onRemove(taskId: number) {
     this.store.dispatch(TasksActions.removeTask({ taskId }));
   }
 
   addNewTask() {
-        this.visible = true;
+    this.visible = true;
   }
 
   ngOnInit(): void {
-      this.serviceTasksList.getTasks().subscribe (
-        (tasks: Task[])=> 
+    this.serviceTasksList
+      .getTasks()
+      .subscribe((tasks: Task[]) =>
         this.store.dispatch(TasksListActions.retrievedTaskList({ tasks }))
-      )
-
-      this.tasks$ = this.store.select(selectTaskList).pipe(
-        map((tasks) => [...tasks])
       );
 
-      // this.tasks$?.subscribe(tasks => {
-      //   console.log('Текущие Store:', tasks);
-      // });
+    this.tasks$ = this.store
+      .select(selectTaskList)
+      .pipe(map((tasks) => [...tasks]));
 
+    // this.tasks$?.subscribe(tasks => {
+    //   console.log('Текущие Store:', tasks);
+    // });
   }
-
 }
--- src/app/state/tasks.reducer.ts
+++ src/app/state/tasks.reducer.ts
@@ -3,17 +3,14 @@ import { Task } from "../modules/task.model";
 import { TasksActions, TasksListActions } from "./tasks.actions";
 
 export const initialState: ReadonlyArray<Task> = [];
-//извлекаем(читаем) состояние task
+//read task state
 export const tasksListReducer = createReducer(
   initialState,
-  on(TasksListActions.retrievedTaskList, (_state, { tasks }) => tasks), //выводим весь массив
+  on(TasksListActions.retrievedTaskList, (_state, { tasks }) => tasks), //get all tasks list
   on(TasksActions.removeTask, (state, { taskId }) => 
-    state.filter((task) => task.id !== taskId) // Удаляем задачу с данным ID
+    state.filter((task) => task.id !== taskId) // delete task byId
   ),
   on(TasksActions.addTask, (state, { task }) => {
-    // if (state.indexOf(task) > -1) {
-    //    return state;
-    // }
     return [...state, task];
   }),
 );
